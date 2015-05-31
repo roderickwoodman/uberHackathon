196a9e40eb55b5b3cfa91b6f8653e2cc7798f020
Trevor Hall <craftelectric@Trevors-MBP.local.tld>
2015-05-30 18:10:38 -0700

    Back to square 1

---
+++ config.js
@@ -0,0 +1,9 @@
+
+// This is a sample config.js file where you will put the tokens that you get from Uber
+// remember to change these AND change the name of the file to config.js after you are done
+module.exports = {
+  ClientID: "hKHEkXECT37s8jTbdgIFCN2xPXoLKtk9",
+  ClientSecret: "cdBcL2AXAwIOCAgviw0eZjpH_7OdzNX7nm0on3LE",
+  ServerID: "UxGTRNENSZGKqHuSHRBjpn0dd9OnGIfSWRj1sUBF"
+}
+ 
